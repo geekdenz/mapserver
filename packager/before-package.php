e59486201db5ff47b9fb488df593b94d6f8e6ed9
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-06-06 18:10:26 +1200

    now building, to try: deployment

--- packager/before-package.php
+++ packager/before-package.php
@@ -22,24 +22,13 @@ $apt_deps = array(
     'php5-dev',
     'gcc',
     'make',
-    //'apache2',
-    //'apache2-mpm-worker',
-    //'libapache2-mod-fastcgi',
-    //'',
 );
 x("sudo apt-get install -y ". implode(' ', $apt_deps));
 e("Compiling MapServer...");
 x("mkdir build");
 $prefix = "'". trim(`pwd`) ."/build'";
-//x("./configure --exec-prefix=$prefix --prefix=$prefix --with-proj --with-geos --with-gdal=/usr/local/bin/gdal-config ");
-
-/*
-proj geos gdal ogr postgis cairo wmsclient wfs wfsclient wcs curl-config xml2-config sos fastcgi freetype gd jpeg png kml threads libsvg-cairo php
-*/
 x("./configure --with-proj --with-geos --with-gdal ".//=/usr/local/bin/gdal-config ".
     "--with-ogr --with-postgis --with-cairo --with-wmsclient --with-wfs --with-wfsclient --with-wcs --with-curl-config ".
     "--with-xml2-config --with-sos --with-fastcgi --with-freetype --with-gd --with-jpeg --with-png --with-kml --with-threads ".
     "--with-libsvg-cairo --with-php=/usr/bin/php-config ".
     "&& make clean && make && make install DESTDIR=$prefix && echo SUCCESSFULLY COMPILED");
-// x("./configure --with-wfs --with-postgis= --with-cairo --with-kml --with-fastcgi --prefix='". trim(`pwd`) ."/build' && make clean && make && make install && echo SUCCESS");
-//x("./configure --with-wfs --with-postgis= --with-cairo --with-kml --with-fastcgi --prefix='". trim(`pwd`) ."/build' && make clean && make && make install && echo SUCCESS");
--- packager/config.php
+++ packager/config.php
@@ -12,11 +12,12 @@ return array(
         'files' => array(
             //'*' => '/usr/local/php-packager/',
             //'packager/*' => '/usr/local/php-packager/packager/
+            'build/*' => '/',
         ),
         'license' => 'GPL v3',
         //'user' => 'heuert', // optional. The user to upload the deb package, `whoami` by default
         //'repository' => 'repository.test:/var/www/dists/stable/main/binary-amd64', // ssh url of repository to put this in
-        'repository' => 'ubuntu:/tmp/',
+        'repository' => 'repository.test.zen.landcareresearch.co.nz:/var/www/dists/precise/main/binary-amd64',
         'url' => 'http://mapserver.org',
         'vendor' => 'Regents of the University of Minnesota & Landcare Research',
 

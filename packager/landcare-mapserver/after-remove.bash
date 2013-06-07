ec5f3e66119914a4ddc955d8f617829dfee138a9
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-06-07 17:46:28 +1200

    landcare: removed bash script

--- packager/landcare-mapserver/after-remove.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf /var/cache/deb/landcare-mapserver
-
-rm -rf /var/cache/deb/landcare-mapserver
-
-rm -rf /var/cache/deb/landcare-mapserver
-
-rm -rf /var/cache/deb/landcare-mapserver

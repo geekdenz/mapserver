7a15333d056c8cca3eb490b5d9e75f3dabde3143
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2013-06-12 11:18:21 +1200

    automatic commit: package.php version v0.1.16

--- mapscript/php/shape.c
+++ mapscript/php/shape.c
@@ -1274,7 +1274,7 @@ void mapscript_create_shape(shapeObj *shape, parent_object parent, php_layer_obj
         add_assoc_string(php_shape->values, php_layer->layer->items[i], php_shape->shape->values[i], 1);
         fprintf(stderr, "String It: i=%d lval=%d str=\n",
 		i,
-		php_shape->shape->values[i].value.lval);//,
+		php_shape->shape->values[i]);//.value.lval);//,
 		//php_shape->shape->values[i].value.str.val);
       }
       //mapscript_throw_exception(str TSRMLS_CC, i, php_shape->shape->numvalues);
--- packager/before-package.php
+++ packager/before-package.php
@@ -48,3 +48,4 @@ x("mkdir -p $root/etc/php5/apache2/conf.d");
 file_put_contents($root .'/etc/php5/apache2/conf.d/mapscript.ini', "; loading mapscript extension\n".
         "extension=/usr/lib/php5/20090626/php_mapscript.so");
 x("cp -Rp $root/* $pwd/packager/root");
+die();
